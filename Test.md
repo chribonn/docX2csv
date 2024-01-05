d2829f884e65051248003376ff73f9830bbd8fcc
chribonn <chribonn@gmail.com>
2024-01-05 14:50:28 +0100

    Synch

---
+++ Test.md
@@ -0,0 +1,11 @@
+| Style Name   | Text                                                            | Section | Page |
+|--------------|-----------------------------------------------------------------|---------|------|
+| ColumnAStyle | ColumnAStyle: Bla Bla Bla This is in Section 1 / Page 1         | 1       | 1    |
+| ColumnBStyle | ColumnBStyle: Ding Dong This is in Section 1 / Page 1           | 1       | 1    |
+| ColumnCStyle | ColumnsCStyle This is in Section 1 / Page 1                     | 1       | 1    |
+| ColumnAStyle | <indent>Nothin here This is in Section 2 / Page 2               | 2       | 2    |
+| ColumnAStyle | •    Ds’lkf;sdlmvfsa This is in Section 2 / Page 2              | 2       | 2    |
+| ColumnAStyle | <right align>Fdsafsdafdsdf This is in Section 2 / Page 2        | 2       | 2    |
+| ColumnBStyle | ColumnBStyle: Ding Dong This is in Section 4 / Page 2           | 4       | 2    |
+| ColumnAStyle | ColumnAStyle: This is in Section 4 / Page 3                     | 4       | 3    |
+| ColumnBStyle | <font size change> ColumnBStyle:  This is in Section 4 / Page 4 | 4       | 4    |
--- listSections.py
+++ listSections.py
@@ -3,46 +3,21 @@ from docx import Document
 docName = "Test.docx"
 
 document = Document(docName)
+pg = 1
+print ('{0:<15} | {1:<30} | {2:>6} | {3:>6}'.format('Style Name', 'Text', 'Section', 'Page'))
 
 for para in document.paragraphs:
-    print (para.text)
-    print (para.style.name)
-    print (para.style.type)
-    print (para.contains_page_break)
-
-    
-# for para in document.paragraphs:
-#     for run in para.runs:
-#         print (run.text)
-#         print (run.style)
-#         print (run.contains_page_break)
-
-    
+    if para.contains_page_break:
+        pg += 1
+    else:
+        for run in para.runs:
+            if para.contains_page_break:
+                pg += 1
 
 
-
-# for paragraph in document.paragraphs:
-#    print (paragraph.name, paragraph.text)
-
-
-
-
-
-
-def list_sections_in_word_document(file_path):
-    """
-    Reads in a word document and lists all the sections in the document.
-
-    Args:
-    file_path (str): The file path of the word document.
-
-    Returns:
-    list: A list of section names in the document.
-    """
-    sections = []
-    doc = docx.Document(file_path)
-    for paragraph in doc.paragraphs:
-        if paragraph.style.name == 'Heading 1':
-            sections.append(paragraph.text)
-    return sections
+    styleName = (para.style.name)
+    if styleName.startswith('Column'):
+        print ('{0:<15} | {1:<30} | {2:>6} | {3:>6}'.format(styleName, para.text[0:30], ' ', pg))
+        print ('-' * 80)
+        
 
