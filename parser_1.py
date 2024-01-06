49da08a8b9d0e4c6545ab2d3e5cf1d5509ed32e0
Alan Bonnici <chribonn@gmail.com>
2024-01-06 12:33:02 +0100

    Implemented the Style processing

--- parser_1.py
+++ parser_1.py
@@ -1,12 +1,40 @@
 # https://docs.python.org/3/library/xml.etree.elementtree.html
 import xml.etree.ElementTree as ET
-# tree = ET.parse(r'D:\chrib\PythonProjects\summariseDocx\data.xml')
-tree = ET.parse(r'D:\chrib\PythonProjects\summariseDocx\Test\word\document.xml')
+
+# params
+fileName = r'.\Test\word\document.xml'
+
+#constants
+NS_URI = "http://schemas.openxmlformats.org/wordprocessingml/2006/main"
+NW_URI_TAG = '{' + NS_URI + '}'
+
+
+def proc_pPr(branch):
+    # print (branch.tag, branch.attrib)
+    name = branch.find(NW_URI_TAG + 'pStyle')
+    if name is not None:
+        style = name.get(NW_URI_TAG + 'val')
+    else:
+        style = None
+        
+    return style
+
+
+tree = ET.parse(fileName)
 
 root = tree.getroot()
 
-ET.register_namespace("w", "http://schemas.openxmlformats.org/wordprocessingml/2006/main")
-ns = {"w": "http://schemas.openxmlformats.org/wordprocessingml/2006/main"}
+
+ET.register_namespace("w", NS_URI)
+ns = {"w": NS_URI}
 # ET.dump(tree)
+
 for x in root.findall('.//w:p', ns):
-    print (x.text, " ", x.tag)
+    # print (x)
+    for y in x:
+        if y.tag == NW_URI_TAG + 'pPr':
+            result = proc_pPr(y) or (None)
+            if result is not None:
+                print (result)
+            
+
