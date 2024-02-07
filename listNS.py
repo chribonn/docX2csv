4ad31f70978aa443b98e96e63c48f766ddf0c5b5
Alan C Bonnici <chribonn@gmail.com>
2024-02-07 17:19:58 +0100

    Delete listNS.py

--- listNS.py
+++
@@ -1,9 +0,0 @@
-import xml.etree.ElementTree as ET
-
-fileName = r'.\Test\word\document.xml'
-
-my_namespaces = dict([
-    node for (_, node) in ET.iterparse(fileName, events=['start-ns'])
-])
-
-print (my_namespaces["wpc"])
